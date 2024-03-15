e7bd6b9afa91eca67287089ab8e1b233f4698cbd
Abhinav Sharma <abhi.sharma1423@gmail.com>
2024-03-15 18:17:32 +0100

    added the function for weep plot

--- WEEP/weep.py
+++ WEEP/weep.py
@@ -11,11 +11,11 @@ def weep_batch(df:pd.DataFrame):
     '''
     applying weep to a set of wsis
     :param df: dataframe expected to include the columns:
-    df['slide_name'] <- slide names ,
-    df['tile_filename'] <- tile names,
-    df['pred_scores'] <- tile-level prediction scores e.g., class probabilities,
-    df['threshold'] <- classification threshold for that slide 
-    :return: df with selected tiles for each WSI and the tile-level prediction scores
+    df['slide_name'] <- string: slide names ,
+    df['tile_filename'] <- string of tile names,
+    df['pred_scores'] <- float: tile-level prediction scores e.g., class probabilities,
+    df['threshold'] <- float: classification threshold for that slide
+    :return: df with selected tiles for each WSI and the tile-level prediction score
     '''
     # list of df containing the selected tiles for each WSI
     dfs = []
@@ -33,6 +33,7 @@ def weep_batch(df:pd.DataFrame):
         df_selected_tiles['tile_filename'] = tile_names
         df_selected_tiles['pred_scores'] = pred_scores
         df_selected_tiles['slide_name'] = slide
+        df_selected_tiles['threshold'] = threshold
         dfs.append(df_selected_tiles)
 
     # concatenating the dfs containing the selected tiles for each wsi
@@ -40,8 +41,6 @@ def weep_batch(df:pd.DataFrame):
     df_selected = df_selected.reset_index(drop=True)
 
     return df_selected
-
-
 
 
 def weep_per_slide(df: pd.DataFrame, threshold):
@@ -76,6 +75,42 @@ def weep_per_slide(df: pd.DataFrame, threshold):
 
     return tile_names, pred_scores
 
+def weep_plot(df:pd.DataFrame):
+    '''
 
+    :param df: df['slide_name'] <- string: slide names ,
+    df['tile_filename'] <- string: tile names,
+    df['pred_scores'] <- tile-level prediction scores e.g., class probabilities,
+    df['threshold'] <- float: classification threshold (same for all the slides)
+    :return: weep plot i.e. line plot to observe the selection of tiles using WEEP
+    '''
+    # retrieving the selected tiles using WEEP for the wsis in given df
+    df_selected = weep_batch(df)
+    wsi_list = df.slide_name.unique().tolist()
+    for slide in tqdm(wsi_list):
+        # storing the percentage of the selected tiles for each wsi
+        percentage = []
+        prediction_scores = []
+        df_tmp = df_selected.loc[df_selected['slide_name'] == slide]
+        df_tmp_1 = df.loc[df['slide_name'] == slide]
+        # sorting the pred scores for the selected tiles for the plot
+        df_tmp = df_tmp.sort_values(by='pred_scores', ascending=False)
+        # iterative plotting the percentage of removed tiles vs the pred scores
+        for i in range(len(df_tmp)):
+            percentage.append(((i+1)/len(df_tmp_1))*100)
+            prediction_scores.append(df_tmp.iloc[i]['pred_scores'])
+        # plotting the line plot for that wsi
+        plt.plot(percentage, prediction_scores, linewidth=1, alpha = 0.2)
 
+    plt.axhline(y=df.iloc[0]['threshold'], label='classification threshold')
+    plt.ylim(0.5, 1.0)
+    # plt.xlim(0, 100)
+    plt.xlabel("Removal percentage of top ranked tiles", fontsize=12)
+    plt.ylabel("Slide-level prediction score", fontsize=12)
+    # plt.box(False)
+    # plt.spines['top'].set_visible(False)
+    # plt.spines['right'].set_visible(False)
+    # plt.title("Kappa vs Lower thresholds with upper_thres={}".format(upper_thres))
+    plt.legend(loc="lower right", prop={'size': 8, 'weight': 'bold'})
+    plt.show()
 
